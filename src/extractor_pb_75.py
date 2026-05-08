96a432303d4ea5e39e7a7fb1545b7a1a5cf2208b
Casey Law <caseyjlaw@gmail.com>
2026-05-08 19:15:31 +0000

    copying in gregg's primary beam reading functions.  duplicate code that should ultimately be made into a library for common use!

---
+++ src/extractor_pb_75.py
@@ -0,0 +1,8 @@
+"""
+Compatibility shim for historical imports.
+
+Prefer importing from ``image_plane_correction.extractor_pb_75``.
+"""
+
+from image_plane_correction.extractor_pb_75 import *  # noqa: F403
+
---
+++ src/pb_correct.py
@@ -0,0 +1,8 @@
+"""
+Compatibility shim for historical imports.
+
+Prefer importing from ``image_plane_correction.pb_correct``.
+"""
+
+from image_plane_correction.pb_correct import *  # noqa: F403
+
